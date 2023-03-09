3af38036bbb7fc295a68f5b1270aab88e525524f
baneshispace <baneshispace@gmail.com>
2023-03-09 15:22:06 +0330

    change env to use in file argument

--- lib/initSupabase.ts
+++ lib/initSupabase.ts
@@ -1,7 +1,10 @@
-import { createClient } from '@supabase/supabase-js'
-import { Database } from './schema'
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "./schema";
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-)
+  // process.env.NEXT_PUBLIC_SUPABASE_URL,
+  "https://your-project.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlempzYWN2dGhlZHl2aXFtanN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgzNDk2OTcsImV4cCI6MTk5MzkyNTY5N30.SfpwEmwp4yp0i7EFVikEHxo8jLboY3EV15Qj8tfjIio"
+
+  // process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
